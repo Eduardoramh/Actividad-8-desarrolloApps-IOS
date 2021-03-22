a0fd8235c1cb35071cb3c446116925372e90f95f
Eduardoramh <45518654+Eduardoramh@users.noreply.github.com>
2021-03-22 13:15:26 -0600

    Add files via upload

---
+++ Actividad8_ERH/Actividad8_ERH/Actividad8_ERH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Actividad8_ERH
+//
+//  Created by user183810 on 3/22/21.
+//  Copyright © 2021 user183810. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
